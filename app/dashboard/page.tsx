0bc878b0325fefed5fe20b669ec9109802e2e103
Dorian1218 <dmitchell1663@gmail.com>
2025-01-21 23:50:55 -0500

    created dashboard page where chat will be

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+export default function page() {
+    return (
+       <div className='p-5 flex flex-col items-center justify-center h-screen'>
+            <p>Dashboard</p>
+       </div>
+    )
+}
